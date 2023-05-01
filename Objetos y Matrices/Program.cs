a2c1c2b50b7ee2a68a99b8d4495d6cbde22ab8d0
Keinaro <georgepor13@gmail.com>
2023-05-01 12:47:45 -0600

    Segundo Ejercicio

--- Objetos y Matrices/Program.cs
+++ Objetos y Matrices/Program.cs
@@ -44,5 +44,29 @@ class Persona
 
         Persona persona = new Persona(nombre, apellido, edad, altura);
         persona.Datos();
+
+        Console.WriteLine("\nIngrese el primer número de multiplicar: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingrese el segundo número de multiplicar: ");
+        int y = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine();
+        int[,] matriz = new int[x, y];
+
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                matriz[i, j] = (i + 1) * (j + 1);
+            }
+        }
+
+        for (int i = 0;i < x; i++)
+        {
+            for (int j = 0;j < y; j++)
+            {
+                Console.Write(matriz[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
     }
 }
